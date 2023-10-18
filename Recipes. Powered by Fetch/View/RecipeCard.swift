38bf5977a087e09dbeee9fb3f430fceca5644f9b
Tevin Jones <Stratmon.Jones@gmail.com>
2023-10-17 19:49:32 -0500

    staging here because i mapped all the outlets to the ViewController and want to save that progress. Was rather tedious.

--- Recipes. Powered by Fetch/View/RecipeCard.swift
+++ Recipes. Powered by Fetch/View/RecipeCard.swift
@@ -16,19 +16,70 @@ class RecipeCard: UIViewController{
     
     @IBOutlet weak var imageView: UIImageView!
     
+
+    @IBOutlet weak var instructionsLabel: UILabel!
+    
+    @IBOutlet weak var mLabel1: UILabel!
+    
+    @IBOutlet weak var iLabel1: UILabel!
+    
+    @IBOutlet weak var mlabel2: UILabel!
+    
+    @IBOutlet weak var iLabel2: UILabel!
+    
+    @IBOutlet weak var mlabel3: UILabel!
+    
+    @IBOutlet weak var ilabel3: UILabel!
+    
+    @IBOutlet weak var mlabel4: UILabel!
+    
+    @IBOutlet weak var ilabel4: UILabel!
+    
+    @IBOutlet weak var mlabel5: UILabel!
+    
+    @IBOutlet weak var iLabel5: UILabel!
+    
+    @IBOutlet weak var mLabel6: UILabel!
+    
+    @IBOutlet weak var iLabel6: UILabel!
+    
+    @IBOutlet weak var mLabel7: UILabel!
+    
+    @IBOutlet weak var iLabel7: UILabel!
+    
+    @IBOutlet weak var mLabel8: UILabel!
+    
+    @IBOutlet weak var iLabel8: UILabel!
+    
+    @IBOutlet weak var mLabel9: UILabel!
+    
+    @IBOutlet weak var iLabel9: UILabel!
+    
+    @IBOutlet weak var mLabel10: UILabel!
+    
+    @IBOutlet weak var iLabel10: UILabel!
+    
+    @IBOutlet weak var mLabel11: UILabel!
+    
+    @IBOutlet weak var iLabel11: UILabel!
+    
+    
+    @IBOutlet weak var mLabel12: UILabel!
+    
+    @IBOutlet weak var iLabel12: UILabel!
+    
+    
+    
     var recipeCache: MealInfo?
-    var name: String?
-    var imageURL: URL?
     var instructions: String?
-    var ingredients: String?
     var delegate: NetworkRequestsDelegate?
     var networkRequests: NetworkRequests?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         networkRequests = NetworkRequests()
         networkRequests?.delegate = self
+        
         nameLabel.text = recipeCache?.strMeal
         imageView.kf.setImage(with: recipeCache?.strMealThumb)
     }
@@ -38,6 +89,27 @@ class RecipeCard: UIViewController{
         DispatchQueue.main.async {
             self.nameLabel.text = self.recipeCache?.strMeal
             self.imageView.kf.setImage(with: self.recipeCache?.strMealThumb)
+            
+            self.instructionsLabel.text = self.recipeCache?.strInstructions
+            self.mLabel1.text = self.recipeCache?.strMeasure1
+            self.iLabel1.text = self.recipeCache?.strIngredient1
+            self.mlabel2.text = self.recipeCache?.strMeasure2
+            self.mlabel3.text = self.recipeCache?.strMeasure3
+            self.ilabel3.text = self.recipeCache?.strIngredient3
+            self.mlabel4.text = self.recipeCache?.strMeasure4
+            self.ilabel4.text = self.recipeCache?.strIngredient4
+            self.mlabel5.text = self.recipeCache?.strMeasure5
+            self.iLabel5.text = self.recipeCache?.strIngredient5
+            self.mLabel6.text = self.recipeCache?.strMeasure6
+            self.iLabel6.text = self.recipeCache?.strIngredient6
+            self.mLabel7.text = self.recipeCache?.strMeasure7
+            self.iLabel8.text = self.recipeCache?.strIngredient8
+            self.mLabel9.text = self.recipeCache?.strMeasure9
+            self.iLabel9.text = self.recipeCache?.strIngredient9
+            self.mLabel10.text = self.recipeCache?.strMeasure10
+            self.iLabel10.text = self.recipeCache?.strIngredient10
+            self.mLabel11.text = self.recipeCache?.strMeasure11
+            self.mlabel2.text = self.recipeCache?.strMeasure12
         }
     }
 }
